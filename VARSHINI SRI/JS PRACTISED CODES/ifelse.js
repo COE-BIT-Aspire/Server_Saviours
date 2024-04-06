9bf0d968085b47e5a9e2181b043e07367a9d34d2
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-04-06 10:51:08 +0530

    Add files via upload

---
+++ VARSHINI SRI/JS PRACTISED CODES/ifelse.js
@@ -0,0 +1,32 @@
+// Is It Raining Outside?
+var rain = false
+if(rain){
+    console.log("Take An Umbrella")
+}
+else{
+    console.log("Enjoy The Sunshine")
+}
+
+// Did You Finish Your Homework?
+var homework = true
+if(homework){
+    console.log("Great Job!")
+}
+else{
+    console.log("Finish Your Homework Before Playing")
+}
+
+// What Is The Season?
+var season = "summer"
+if(season == "spring"){
+    console.log("Enjoy The Blooming Flowers")
+}
+else if(season == "summer"){
+    console.log("Have Fun In The Sun")
+}
+else if(season == "autumn"){
+    console.log("Admire The Colorful Leaves")
+}
+else if(season == "winter"){
+    console.log("Bundle Up And Stay Warm")
+}
