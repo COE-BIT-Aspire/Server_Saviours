6833cf0beb3afd7388ee54d041069b5612aa84c9
Jaikanth <106078229+Jaikanth-Sivakumar@users.noreply.github.com>
2024-04-06 08:52:56 +0530

    Add files via upload

---
+++ jaikanth/project html & js/javascript for railway.js
@@ -0,0 +1,27 @@
+class Login {
+  constructor() {
+    this.loginForm = document.getElementById("loginForm");
+    this.errorMessage = document.getElementById("errorMessage");
+    this.loginForm.addEventListener("submit", this.handleFormSubmit.bind(this));
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    if (username === "admin" && password === "admin123") {
+      window.location.href = "railway reservation.html";
+    } else {
+      this.errorMessage.innerText = "Invalid username or password.";
+    }
+  }
+}
+window.addEventListener("DOMContentLoaded", () => {
+  new Login();
+});
+
+
+
+function bookTicket(trainNumber) {
+  alert("Ticket booked for Train Number: " + trainNumber);
+}
