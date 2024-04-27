68b73cf9bfe6c33e697f7bfc5a45860e5a15505f
Jaikanth Sivakumar <106078229+Jaikanth-Sivakumar@users.noreply.github.com>
2024-04-27 12:24:47 +0530

    Update Array.ts

--- JAIKANTH S/Typescript/Array.ts
+++ JAIKANTH S/Typescript/Array.ts
@@ -1,7 +1,7 @@
-let arr1 = [ 16,1,14,34];
-let arr2 = [ 42, 53, 66,89];
+let arr1 = [ 1, 2, 3, 4];
+let arr2 = [ 5, 6, 7, 8];
 let copy_arr = [...arr1];
-let new_arr = [...arr1, 72, 83,78];
+let new_arr = [...arr1, 9, 10];
 let merged_arr = [...arr1, ...arr2];
 
 console.log(arr1); 
