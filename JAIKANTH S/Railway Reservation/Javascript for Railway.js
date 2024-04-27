280d01d1ba6cdd72a36c19c7a51dbed294535d00
Jaikanth Sivakumar <106078229+Jaikanth-Sivakumar@users.noreply.github.com>
2024-04-27 12:18:24 +0530

    Add files via upload

---
+++ JAIKANTH S/Railway Reservation/Javascript for Railway.js
@@ -0,0 +1,30 @@
+class Login {
+  constructor() {
+    this.loginForm = document.getElementById("loginForm");
+    this.errorMessage = document.getElementById("errorMessage");
+    this.loginForm.addEventListener("submit", this.handleFormSubmit.bind(this));
+  }
+
+  handleFormSubmit(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    if (username === "user" && password === "user123") {
+      window.location.href = "user.html";
+    }
+    else if (username === "admin" && password === "admin123") {
+      window.location.href = "admin.html";
+    } else {
+      this.errorMessage.innerText = "Invalid username or password.";
+    }
+  }
+}
+window.addEventListener("DOMContentLoaded", () => {
+  new Login();
+});
+
+
+
+function bookTicket(trainNumber) {
+  alert("Ticket booked for Train Number: " + trainNumber);
+}
---
+++ JAIKANTH S/Typescript/Set.ts
@@ -0,0 +1,12 @@
+let set = new Set();
+set.add(1);
+set.add(true);
+set.add("jai");
+
+set.has(1);
+
+set.size;
+
+set.delete(1);
+ 
+set.clear();   
