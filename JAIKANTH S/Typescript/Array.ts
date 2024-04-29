183bcc0f3ad6c2caafd2c4f63b968f8da36558c8
Jaikanth Sivakumar <106078229+Jaikanth-Sivakumar@users.noreply.github.com>
2024-04-29 10:54:47 +0530

    Update Array.ts

--- JAIKANTH S/Typescript/Array.ts
+++ JAIKANTH S/Typescript/Array.ts
@@ -9,3 +9,14 @@ console.log(arr2);
 console.log(copy_arr);
 console.log(new_arr);
 console.log(merged_arr);
+
+var employeeNames = [ 'Jai', 'Sri', 'Ram', 'Hari', 'San' ];
+console.log("To find a employee working in our Company");
+var empName= prompt('Please enter the name.');
+
+if(employeeNames.includes('empName')){
+    console.log("He is working in this company");
+}
+else{
+    console.log("He is not our employ");
+}
