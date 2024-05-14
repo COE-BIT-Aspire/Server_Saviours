c0ea569198e24812edf32f6e9c853608e29f4998
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-05-14 21:11:07 +0530

    Add files via upload

---
+++ VARSHINI SRI/ANGULAR/app/shop/shop.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {Shop} from './shop.model';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-shop',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.css'
+})
+export class ShopComponent {
+  shop:Shop;
+  constructor(){
+    this.shop = new Shop("Books",100,"Bangalore");
+  }
+}
