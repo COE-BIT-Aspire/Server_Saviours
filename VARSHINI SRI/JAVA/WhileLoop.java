5fdff7e5cd05ab67b419e2fdf48d2b4cdf7ff1ee
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-06-03 13:32:33 +0530

    Delete VARSHINI SRI/JAVA directory

--- VARSHINI SRI/JAVA/WhileLoop.java
+++
@@ -1,9 +0,0 @@
-class WhileLoop{
-    public static void main(String args[]){
-        int start = 1;
-        while(start<=5){
-            System.out.println("Welcome To Aspire Systems !");
-            start+=1;
-        }
-    }
-}
