6b0ed79093a4993c13ca0af1bd4d4c35165f740a
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-06-03 19:49:52 +0530

    Delete VARSHINI SRI/JAVA directory

--- VARSHINI SRI/JAVA/MutableString.java
+++
@@ -1,8 +0,0 @@
-class MutableString{
-    public static void main(String args[]){
-        StringBuffer sb = new StringBuffer("Aspire");
-        sb.append(" Systems");
-        sb.insert(0,"Varshini ");
-        System.out.println(sb);
-    }
-}
