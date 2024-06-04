d53144d9824562fee4cf53d9edf7f902d9889117
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-06-04 17:33:52 +0530

    Delete VARSHINI SRI/JAVA directory

--- VARSHINI SRI/JAVA/DoWhile.java
+++
@@ -1,9 +0,0 @@
-class DoWhile{
-    public static void main(String args[]){
-        int start = 1;
-        do{
-            System.out.println("Welcome To Aspire Systems !");
-            start += 1;
-        }while(start<=5);
-    }
-}
--- VARSHINI SRI/JAVA/FinalVariable.java
+++
@@ -1,9 +0,0 @@
-
-
-public class FinalVariable {
-    public static void main(String[] args) {
-        final int num = 6;
-        //num = 5;
-        System.out.println(num);
-    }
-}
