66976466f59925e7cee0b79a949bc4c6bcd740d0
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-06-08 20:34:59 +0530

    Delete VARSHINI SRI/TYPESCRIPT directory

--- VARSHINI SRI/TYPESCRIPT/map.ts
+++
@@ -1,3 +0,0 @@
-const numbers = [1, 2, 3, 4, 5];
-const doubledNumbers = numbers.map(num => num * 2);
-console.log(doubledNumbers); 
--- VARSHINI SRI/TYPESCRIPT/set.ts
+++
@@ -1,8 +0,0 @@
-let set = new Set();
-set.add("Jaya");
-set.add("Saraswathi");
-set.add("Savitha");
-set.add("Silpa");
-set.add("Snigdha");
-set.add("Anitha");
-console.log(set);
