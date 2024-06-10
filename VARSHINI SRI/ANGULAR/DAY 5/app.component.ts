b10b10f44b2a0027cd22152a4afedd9d91ef4f86
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-06-10 13:51:59 +0530

    Add files via upload

---
+++ VARSHINI SRI/ANGULAR/DAY 5/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { ShopComponent } from './shop/shop.component';
+import { FormsModule } from '@angular/forms';
+import { ProductComponent } from './product/product.component';
+import { AccountComponent } from './account/account.component';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,StudentComponent,FormsModule,ShopComponent,ProductComponent,AccountComponent,RouterLink],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'myFirstAngularApp';
+}
---
+++ VARSHINI SRI/ANGULAR/DAY 5/student/student.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  standalone: true,
+  imports: [],
+  templateUrl: './student.component.html',
+  styleUrl: './student.component.css'
+})
+export class StudentComponent {
+  studentName: String = "Varshini";
+  studentMark: number = 483;
+  joinDate: Date = new Date();
+  message: String = "This is event binding";
+  fontColor: String = "Blue";
+
+  displayMessage(){
+    alert(this.message);
+  }
+}
