cbc255718b8241b0e6835d69836c1227ff45049e
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-06-17 14:29:27 +0530

    Add files via upload

---
+++ VARSHINI SRI/REACT JS/DAY 5/host-demo/GuestGreeting.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function GuestGreeting(props) {
+  return (
+    <div>GuestGreeting
+      <h1 style = {{color:'red'}}>Hi this is guest user</h1>
+    </div>
+  )
+}
+
