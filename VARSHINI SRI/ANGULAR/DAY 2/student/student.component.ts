b28dca89040a364738c9763467b563e21ae3feda
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-06-17 21:07:17 +0530

    Add files via upload

---
+++ VARSHINI SRI/ANGULAR/DAY 2/student/student.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student',
+  standalone: true,
+  imports: [],
+  templateUrl: './student.component.html',
+  styleUrl: './student.component.css'
+})
+export class StudentComponent {
+  studentName: String = "Varshini";
+  studentMark: number = 483;
+  joinDate: Date = new Date();
+  message: String = "This is event binding";
+
+  displayMessage(){
+    alert(this.message);
+  }
+}
