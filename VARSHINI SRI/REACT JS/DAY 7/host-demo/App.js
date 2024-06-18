7a71e7f947a4a352b6a84683846ee741fb18c495
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-06-18 12:41:21 +0530

    Add files via upload

---
+++ VARSHINI SRI/REACT JS/DAY 7/host-demo/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import {Route,Link,BrowserRouter,Routes} from 'react-router-dom';
+import Nav from './Nav';
+import Home from './Home';
+import ContactUs from './ContactUs';
+import Product from './Product';
+import Laptop from './Laptop';
+import ProductList from './ProductList';
+import MountingDemo from './MountingDemo';
+function App() {
+  return (
+    <div>
+      <h1 style = {{color:'orange'}}>Hi! Everyone...</h1>
+      {/* <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path = 'home' element = {<Home/>}/>
+        <Route path = 'contactus' element = {<ContactUs/>}/>
+        <Route path = 'product' element = {<Product/>}/>
+        <Route path = '/product/laptop' element = {<Laptop/>}/>
+      </Routes>
+      </BrowserRouter><br/>
+      <ProductList/> */}
+    <MountingDemo/>
+    </div>
+  );
+}
+export default App;
