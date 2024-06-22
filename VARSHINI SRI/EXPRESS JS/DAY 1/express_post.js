23b711eeea1cf10b86b309969e9f392b6f3130d5
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-06-22 13:45:48 +0530

    Add files via upload

---
+++ VARSHINI SRI/EXPRESS JS/DAY 1/express_post.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var server = express();
+
+server.post("/",(request,response)=>{
+    response.send("<h1>Express js application</h1>")
+})
+
+server.post("/home",(request,response)=>{
+    response.send("<h1>Landing on home page</h1>")
+})
+
+server.listen(5000,()=>{
+    console.log("Express server is waiting for client connection")
+})
