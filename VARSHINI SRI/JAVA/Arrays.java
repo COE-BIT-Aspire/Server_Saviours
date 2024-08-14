1ea1203e01e9568c1acca70f85870789a203773b
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2024-08-14 14:44:44 +0530

    Delete VARSHINI SRI/JAVA directory

--- VARSHINI SRI/JAVA/Arrays.java
+++
@@ -1,9 +0,0 @@
-class Arrays{
-    public static void main(String args[]){
-        String products[] = {"Laptop", "Modem", "Headset", "Charger", "Mouse"};
-        // String products[] = new String[5];
-        for(int start=0; start<=4; start++){
-            System.out.println(products[start]);
-        }
-    }
-}
--- VARSHINI SRI/JAVA/ForLoop.java
+++
@@ -1,7 +0,0 @@
-class ForLoop{
-    public static void main(String args[]){
-        for(int start=1; start<=5; start++){
-            System.out.println("Welcome To Aspire Systems !");
-        }
-    }
-}
