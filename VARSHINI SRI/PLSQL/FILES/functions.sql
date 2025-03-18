3235f0471ac782f47e504f4b14c51c40d7724890
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2025-03-18 15:47:14 +0530

    Delete VARSHINI SRI/PLSQL directory

--- VARSHINI SRI/PLSQL/FILES/functions.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION circle_area (radius NUMBER) 
-RETURN NUMBER IS
-pi  	CONSTANT NUMBER(7,2) :=	3.141;
-area 	NUMBER(7,2);
-BEGIN
-  area := pi * (radius * radius);
-  RETURN area; 
-END;
