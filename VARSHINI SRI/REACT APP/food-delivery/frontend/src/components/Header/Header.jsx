6f90394cf516dc2ba01869836b1d9f2c7789fd7b
Varshini Sri <154018094+Varshini2406@users.noreply.github.com>
2025-06-22 21:32:11 +0530

    Add files via upload

---
+++ VARSHINI SRI/REACT APP/food-delivery/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order Your Favourite Food Here</h2>
+            <p>Welcome to FoodieBay, your go-to food delivery service, bringing delicious meals right to your doorstep in just a few taps. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
